5dee6d3ccc0435e1aa0bc26530d4e7c72c453c4c
Ezhilvannan <leninezhilvannan@gmail.com>
2021-11-09 13:29:56 +0530

    PassWord Functionality

--- src/org/facebook/Login.java
+++ src/org/facebook/Login.java
@@ -1,8 +1,11 @@
 package org.facebook;
 
 public class Login {
+	public static void password() {
+		System.out.println("Password");
+	}
 	
 public static void main(String[] args) {
-	
+	password();
 }
 }
