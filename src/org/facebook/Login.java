b48cd675927701ec1f3ca882fa30815cd2dc86e6
Ezhilvannan <leninezhilvannan@gmail.com>
2021-11-09 13:19:42 +0530

    UserName Functionality

--- src/org/facebook/Login.java
+++ src/org/facebook/Login.java
@@ -1,8 +1,11 @@
 package org.facebook;
 
 public class Login {
+	public static void userName() {
+		System.out.println("UserName");
+	}
 	
 public static void main(String[] args) {
-	
+	userName();
 }
 }
